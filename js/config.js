9052b7c418883550b55e1c94715cf4649a1d1f5d
jorge21028 <51407591+jorge21028@users.noreply.github.com>
2026-08-29 09:40:07 -0400

    Fix API_BASE URL typo in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Cambia esto por la URL real de tu API en Hostinger, ej:
 // const API_BASE = 'https://api.tudominio.com';
 // o si está en una subcarpeta: 'https://tudominio.com/planificacion-api';
-const API_BASE = 'https://planificacion-api.nstitutoifgla.edu.do';
+const API_BASE = 'https://planificacion-api.institutoifgla.edu.do';
